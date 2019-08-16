1b16a40eb79d78b9d51de459b38ada21e16518ab
havanoor <havanoor@gmail.com>
2019-08-16 22:41:35 +0530

    Code For Task 2

---
+++ SpacexAPP/Space/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from . import models
+
+import datetime
+
+import requests
+
+# stores dictionary of required values
+final=[]
+
+
+def spacef(request):
+
+
+    connect=requests.get("https://api.spacexdata.com/v3/launches")
+    result=connect.json()
+    print(connect.status_code)
+    for i in result:
+
+
+        
+        date_time=i['launch_date_utc'][0:10]
+        #print(date_time)
+        #converting date
+        date_obj=datetime.datetime.strptime(date_time ,'%Y-%m-%d')
+        date_obj2=datetime.datetime.strftime(date_obj,'%d-%b-%Y')
+        #print(type(date_obj))   
+        #dictionary of required values
+        res={'flight_number':i['flight_number'],'mission_name':i['mission_name'],'launch_date_utc':(date_obj2),'rocket_name':i['rocket']["rocket_name"],'mission_patch':i['links']['mission_patch']}
+        final.append(res)
+        print(res['launch_date_utc'])
+        #  print(i)
+      
+    return HttpResponse( "<p><h3>%s<h3></p><br>" %i for i in final)
+    
